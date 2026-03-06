76013dddd9661ed04c4c2ef80a9de85e881eeef5
albin170 <albinbinu170@gmail.com>
2026-03-06 21:00:59 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,71 @@
+body{
+font-family:Arial;
+margin:0;
+background:#f4f6f9;
+color:#222;
+}
+
+header{
+background:#1f2937;
+color:white;
+padding:20px;
+text-align:center;
+}
+
+.controls{
+margin-top:15px;
+display:flex;
+justify-content:center;
+gap:10px;
+flex-wrap:wrap;
+}
+
+input,select,button{
+padding:10px;
+border-radius:6px;
+border:none;
+}
+
+main{
+display:grid;
+grid-template-columns:repeat(auto-fit,minmax(260px,1fr));
+gap:20px;
+padding:30px;
+}
+
+.card{
+background:white;
+padding:15px;
+border-radius:10px;
+box-shadow:0 4px 10px rgba(0,0,0,0.1);
+}
+
+.card a{
+text-decoration:none;
+color:#2563eb;
+}
+
+footer{
+text-align:center;
+padding:20px;
+background:#eee;
+}
+
+/* DARK MODE */
+
+.dark{
+background:#111827;
+color:white;
+}
+
+.dark .card{
+background:#1f2937;
+}
+
+.dark header{
+background:black;
+}
+
+.dark footer{
+background:#000;
+}
